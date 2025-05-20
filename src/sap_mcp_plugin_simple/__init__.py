664605d23c670d4a1bee4024ac5c8cf6f8c58d30
Nacho Garay 👾 <iojeda@amazon.com>
2025-05-20 02:03:21 -0700

    feat: change some of the interface, add packaging, add reverse index

--- src/sap_mcp_plugin_simple/__init__.py
+++ src/sap_mcp_plugin_simple/__init__.py
@@ -1,11 +1,21 @@
+from typing import Dict, List
+
 from boto3 import Session
 from strands import Agent
 from strands.models import BedrockModel
 
-from strands_agent_mcp.registry import Registry
+from strands_agent_mcp.registry import AgentEntry
 
 
-def register_plugin(registry: Registry) -> None:
-    registry.register("simple-agent", Agent(
-        model=BedrockModel(boto_session=Session(region_name="us-west-2")))
-    )
+def build_agents() -> List[AgentEntry]:
+    return [
+        AgentEntry(
+            name="simple-agent",
+            agent=Agent(
+                model=BedrockModel(
+                    boto_session=Session(region_name="us-west-2")
+                )
+            ),
+            skills=["general-knowledge"]
+        )
+    ]
